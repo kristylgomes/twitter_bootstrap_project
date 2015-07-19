38fdb4942c37664f0856f022163b5a4007bfe475
Kristyl Macbook Air local <kristylgomes@hotmail.com>
2015-07-19 09:15:36 -0700

    Updates

--- includes/my_javascript.js
+++ includes/my_javascript.js
@@ -1,4 +1,14 @@
 /*  JavaScript Document                      */
 /*  Written by Chris Converse for Udemy.com  */
 
+$(document).ready(function(){
+	$('.carousel').carousel({interval:6000});
+	$('.typeahead').typeahead();
+})
 
+
+function changeSlide(slideNum){
+
+	$('.carousel').carousel('pause');
+	$('.carousel').carousel(slideNum);
+}
